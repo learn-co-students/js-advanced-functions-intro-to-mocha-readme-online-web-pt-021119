8db1427cc9680100dc36e3ec5d8fe9cf996d9896
github username <github email address>
2019-09-14 19:30:18 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function favoriteIceCream(flavor) {
+  return 'I love ' + flavor;
+}
+
+const theTruth = () => true;
+
+const sayHey = () => 'Hey!';
+
+const sayHeyFriend = name => 'Hey, ' + name + '!';
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+const roundDown = (num) => Math.floor(num);
